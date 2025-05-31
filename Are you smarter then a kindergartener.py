cfb4a82d5d3d673777e9d08a21fe1e6a7f6fd916
wxzzgs <154992220+wxzzgs@users.noreply.github.com>
2025-05-31 15:21:59 -0500

    Add files via upload

---
+++ Are you smarter then a kindergartener.py
@@ -0,0 +1,42 @@
+import time
+
+print('1+2 = ')
+
+a = 1
+b = 2
+c = a + b
+print(c)
+time.sleep(1)
+print("This is one example of our math cracker")
+time.sleep(1)
+
+x = 12
+y = 6
+time.sleep(1)
+print('Find x, 12 + 6 = ?')
+time.sleep(5)
+print('five more seconds left')
+time.sleep(5)
+solve = x + y
+print(" ")
+print('The awnser is,')
+print(solve)
+time.sleep(3)
+
+print('Lets raise the value, Friend')
+add = 1
+solve += add
+print(solve)
+time.sleep(1)
+
+print("Now lets do math problems")
+awnser_for_problem = 18
+time.sleep(1)
+print("If john has 6 red apples, 4 blue apples, and 8 orange apples, how many apples does john have in total?")
+time.sleep(5)
+print("5 more seconds left")
+time.sleep(5)
+print(awnser_for_problem)
+time.sleep(1)
+print("More updates in the future!")
+print('credits - Scripter = OceanEmpireRblx')
